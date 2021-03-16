56b64c43c57614727e4fb45e1ae8698ff30ff623
ScaryTiger <64843158+ScaryTiger@users.noreply.github.com>
2021-03-16 22:17:41 +0100

    Bot discord Source Creator tools

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Bot_Discord_source_Creator_by_Scary_Tiger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Per l\'utilizzo di questo programma, devi avere un link della tua licenza ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Inserisci qui il link della tua licenza :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(260, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(292, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ESEMPIO: https://scarytiger.altervista.org/bd/123456789ABCDEFG/";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(228, 83);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(413, 20);
+            this.txtURL.TabIndex = 3;
+            this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(708, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Vai Avanti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(657, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Come posso avere la licenza?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 166);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bot Discord source Creator by Scary Tiger (Mr Tiger)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bot_Discord_source_Creator_by_Scary_Tiger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urltxturl = txtURL.Text + "index.php";
+            bool result = CheckFile(txtURL.Text + "index.php");
+            if (result)
+            {
+                MessageBox.Show("La tua licenza risulta attiva e scadrà tra TOT giorni.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form2.show();
+            }
+            else
+            {
+                MessageBox.Show("Non hai una licenza attiva oppure l'utente non esiste o il link che hai inserito non è valido. Se non e' nessuno di questi problemi, potrebbe essere che sei stato bandito. Contatta Scary Tiger.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private bool CheckFile(string URL)
+        {
+            try
+            {
+
+                HttpWebRequest request = WebRequest.Create(URL) as HttpWebRequest;
+                request.Method = "HEAD";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    return (response.StatusCode == HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+    }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Per avere la licenza di questo software, devi entrare nel server discord di Scary Tiger, inserendo il codice seguente: GKP49SR , e chiedento in privato a Scary Tiger di darti la licenza. Poi lui ti darà un link dove lo devi copiare e incollare qui", "Come avere la licenza per questo software - Istruzioni -- Scary Tiger");
+        }
+
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
